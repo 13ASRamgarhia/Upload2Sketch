54480ff0838aaed26618c852399e13e869d41f26
Amandeep Singh <13asramgarhia@gmail.com>
2024-08-16 16:49:31 -0400

    final commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,38 +31,33 @@ const About = () => {
   const team = [
     {
       id: 1,
-      email: "amandeep.singh36@dcmail.ca",
       first_name: "Amandeep",
       last_name: "Singh",
       sid: 100949940,
     },
     {
       id: 2,
-      email: "janet.weaver@reqres.in",
       first_name: "Angad",
       last_name: "Singh",
-      sid: 100949940,
+      sid: 100945617,
     },
     {
       id: 3,
-      email: "emma.wong@reqres.in",
       first_name: "Jatin",
       last_name: "Sharma",
-      sid: 100949940,
+      sid: 100949281,
     },
     {
       id: 4,
-      email: "eve.holt@reqres.in",
       first_name: "Kunwarpal",
       last_name: "Singh",
-      sid: 100949940,
+      sid: 100951497,
     },
     {
       id: 5,
-      email: "charles.morris@reqres.in",
       first_name: "Vaibhav",
       last_name: "Sharma",
-      sid: 100949940,
+      sid: 100953270,
     },
   ];
 
@@ -76,11 +71,9 @@ const About = () => {
             </div>
             <div className="desc text-justify space-y-6 text-white px-4 laptop:px-48 text-2xl w-full laptop:w-[70%] font-inter">
               <p>
-              Movies have a unique ability to transport us to new worlds, evoke powerful emotions, and spark meaningful conversations. We're on a mission to harness the incredible potential of cinema to enrich lives and inspire imaginations
-              </p>
-
+              We are a passionate team of AI enthusiasts and developers dedicated to merging technology with creativity. Together, we bring diverse skills in frontend development, backend integration, and AI-driven innovation to create cutting-edge digital tools that inspire and empower users.              </p>
               <p className="uppercase">
-              A SINGLE PLATFORM FOR FINDING BEST FAVORITE MOVIES
+              TRANSFORM YOUR PHOTOS INTO STUNNING PENCIL SKETCHES WITH UPLOAD2SKETCH
               </p>
             </div>
           </div>
@@ -89,7 +82,7 @@ const About = () => {
 
       <div className="px-4 laptop:px-48 pt-10">
         <p className="text-headingColor text-2xl font-medium font-inter">
-          ABOUT CINESENSE
+          ABOUT UPLOAD2SKETCH
         </p>
         <div className="space-y-1">
           <div className="h-1 w-56 bg-logoColor"></div>
@@ -101,16 +94,7 @@ const About = () => {
           </div>
           <div className="w-full tablet:w-[70%]">
             <p className="font-inter text-xl text-subHeadingColor mt-6 text-justify">
-              Cinesense is an innovative movie recommendation platform that adapts to your
-              specific cinematic preferences. It uses cutting-edge algorithms to provide personalized
-              movie recommendations based on your interests, ensuring that each viewing
-              experience is tailored to your liking. <br /><br />Whether you're a casual movie enthusiast or a
-              devoted cinephile, our recommendation engine aims to improve your movie-watching
-              experience by delivering handpicked picks that align with your preferences. With each
-              encounter, Cinematic Insights learns from your choices and refines its
-              recommendations to give an ever-improving service. Our recommendations won't be
-              limited to one platform; they'll cover the entire movie universe, including Hollywood,
-              Bollywood, and other OTT services
+            The Image-to-Sketch application is a cutting-edge AI-driven tool designed to transform digital images into high-quality pencil sketches. This application offers users a seamless and intuitive interface, allowing them to upload images and instantly generate detailed sketches that mimic the intricate qualities of hand-drawn art. To enhance user experience, the tool incorporates advanced features like customizable sketch effects and voice command functionality, enabling users to interact with the application hands-free. The application is suitable for a diverse audience, from digital artists and designers looking to streamline their creative process, to social media influencers seeking unique content, and even educational institutions aiming to bridge traditional art techniques with modern technology. Deployed on Netlify and Render, the tool ensures efficient performance and scalability, making it accessible to a broad user base. The development of this application involved overcoming significant challenges, particularly in integrating reliable voice commands and refining the sketch algorithms to achieve an authentic artistic output.
             </p>
           </div>
         </div>
@@ -127,27 +111,9 @@ const About = () => {
         <div className="py-10 flex flex-col tablet:flex-row space-x-0 tablet:space-x-10 space-y-4 tablet:space-y-0">
           <div className="w-full tablet:w-[70%]">
             <p className="font-inter text-justify text-xl text-subHeadingColor mt-6">
-            Our main goal is to revolutionize the movie industry by implementing AI uses cases such as to provide personalized movie recommendations based on your interests, ensuring that each viewing experience is tailored to your liking. We intend to offer diverse range of movie experiences be it based on language, culture, history, genres, and different ways of storytelling. Our AI system will keep on improving and enhancing as we will continue to feed the data back to it and monitor for any bias. also, we will combine the content which spread over different platforms to offer a wide range of choices at one location. Our platform features will not be limited to just recommendation and binge watching. We will be a creating dynamic space where users can provide reviews and participate in social events like Virtual movie carnivals, movie debates etc.
+            Our vision is to become the leading platform for digital artistic transformation, enabling users worldwide to effortlessly create beautiful, hand-drawn-quality sketches from their images. We aspire to inspire creativity by harnessing the power of AI, making advanced digital art tools accessible to everyone, regardless of their technical expertise. By continuously innovating and enhancing our platform, we aim to foster a global community where artistic expression is democratized, bridging the gap between traditional art and digital technology, and empowering users to explore new creative possibilities in a modern, user-friendly environment.
             <br /><br />
-            Our objective is to create an AI-powered application that will identify trends and
-            pattern of user viewing habits to provide personalized movie recommendations
-            tailored to everyone’s tastes. This platform will be the one stop shop for every type of
-            content be it movies, web series, daily soaps, sports, and other categories from all
-            OTT platform this will not only benefits the AI, but customers will have pay for just
-            one membership for a wide variety of content minus the hassle of managing multiple
-            platforms. People who don't want to waste their time watching movies only to find out
-            that the content isn't for them will be among our consumers. Our solution provides
-            recommendations tailored to their liking, Movie Insights, movie review and will have
-            interactive features like Movies fest, sentimental analysis of review, movie debates.
-            We will promote our product by properly working on the content of the website to use
-            the full potential of Search engine optimization and grow organically in early stages,
-            then we can use start investing in digital marketing tools google ads, facebook ads
-            manager and use the power of influencers to increase the visibility of the product
-            online. Features like movie fest and debates will help in promoting product in social
-            media platforms. Our product will easily be accessible to the users as we will create
-            website and apps for both android and apple devices and the uniqueness of our
-            platform will depend on the gamification of the user experience, data collection and
-            virtual social gathering events.
+            Our mission is to revolutionize digital art creation by providing an accessible, AI-powered tool that empowers users of all skill levels to transform their images into stunning pencil sketches. We aim to replicate the intricate details and textures of traditional hand-drawn art, making it easy for everyone—from professional artists to casual hobbyists—to engage in creative expression. Our commitment is to deliver a seamless and user-friendly experience through an intuitive interface and innovative features like voice commands. By doing so, we aspire to inspire a broader audience to explore and enjoy the world of digital sketching, bridging the gap between traditional artistry and modern technology.
             </p>
           </div>
           <div className="w-full tablet:w-[30%] flex my-auto">
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,106 +1,371 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Main = () => {
-  const [imageUrl, setImageUrl] = useState(null); // State for the uploaded image URL
-  const [sketchUrl, setSketchUrl] = useState(""); // State for the generated sketch URL
-  const [loading, setLoading] = useState(false); // State for loading status
-  const [imageFileName, setImageFileName] = useState(""); // State for the image file name
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedImageUrl, setSelectedImageUrl] = useState(null);
+  const [basicSketchUrl, setBasicSketchUrl] = useState(null);
+  const [effectsSketchUrl, setEffectsSketchUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [effectLoading, setEffectLoading] = useState(false);
+  const { transcript, resetTranscript } = useSpeechRecognition();
 
-  // Handle image file upload
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    setImageFileName(file.name);
+  // State variables for input ranges
+  const [brightness, setBrightness] = useState(1);
+  const [sepia, setSepia] = useState(0.5);
+  const [sharpen, setSharpen] = useState(0.5);
+  const [vignette, setVignette] = useState(0.5);
+  const [sepiaIntensity, setSepiaIntensity] = useState(0.5);
+  const [vignetteIntensity, setVignetteIntensity] = useState(0.5);
+  const [sharpenIntensity, setSharpenIntensity] = useState(0.5);
+  const [sketchStyle, setSketchStyle] = useState("default");
+  const [border, setBorder] = useState(false);
+  const [borderSize, setBorderSize] = useState(5);
+  const [borderColor, setBorderColor] = useState("#000000");
+  const [frame, setFrame] = useState(false);
+  const [frameType, setFrameType] = useState("simple");
 
-    if (file) {
-      const url = URL.createObjectURL(file);
-      setImageUrl(url);
-      console.log("Image URL:", url);
-      console.log(imageFileName)
-    }
+  // Check if browser supports speech recognition
+  const isSpeechRecognitionSupported = SpeechRecognition.browserSupportsSpeechRecognition();
+
+  // Handle file change event
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
 
-  // Convert the image to a sketch using an external API
-  const handleSketchConversion = async () => {
-    if (!imageUrl) return;
+  // Create a URL for the selected image
+  useEffect(() => {
+    if (selectedFile) {
+      const url = URL.createObjectURL(selectedFile);
+      setSelectedImageUrl(url);
+    }
+  }, [selectedFile]);
 
+  // Create a basic sketch
+  const createBasicSketch = async () => {
+    setSketchStyle('default')
+    setBorder(false)
+    setBorderSize(5)
+    setBorderColor("#000000")
+    setFrame(false)
+    setFrameType("simple")
     setLoading(true);
+    if (!selectedFile) {
+      alert("Please upload an image first.");
+      setLoading(false);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile, "image.jpg");
 
     try {
       const response = await axios.post(
-        `https://sketch-x6qw.onrender.com/create_sketch_basic/`,
+        "https://sketch-x6qw.onrender.com/create_sketch_basic/",
+        formData,
         {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
           responseType: "blob",
         }
       );
 
-      const blob = response.data;
-      const url = URL.createObjectURL(blob);
-      setSketchUrl(url);
+      if (response.status === 200) {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        setBasicSketchUrl(url);
+        console.log("Basic sketch displayed.");
+      } else {
+        console.error("Failed to create basic sketch.");
+      }
+      setLoading(false);
     } catch (error) {
-      console.error("Error creating sketch", error);
-      setSketchUrl("");
-    } finally {
+      console.error("Error creating basic sketch:", error);
       setLoading(false);
     }
   };
 
-  // // Download the image
-  // const downloadImage = (url, filename) => {
-  //   if (!url) return;
-  //   const link = document.createElement("a");
-  //   link.href = url;
-  //   link.download = filename;
-  //   link.click();
-  // };
+  const convertImageToSketchWithEffects = async () => {
+    if (!selectedFile) {
+      alert("Please select an image file first.");
+      return;
+    }
 
+    try {
+      setEffectLoading(true)
+      // Convert image to byte object
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(selectedFile);
+      reader.onloadend = async () => {
+        const byteObject = reader.result;
+
+        // API call to sketch creation with effects function
+        const effectsSketchUrl = "https://sketch-x6qw.onrender.com/create_sketch_with_effects/";
+        const effectsParams = {
+          brightness: brightness,
+          sepia: sepia > 0 ? "true" : "false",
+          sepia_intensity: sepiaIntensity,
+          vignette: vignette > 0 ? "true" : "false",
+          vignette_intensity: vignetteIntensity,
+          sharpen: sharpen > 0 ? "true" : "false",
+          sharpen_intensity: sharpenIntensity,
+          sketch_style: sketchStyle,
+          border: border ? "true" : "false",
+          border_size: border ? borderSize : "0",
+          border_color: border ? borderColor.replace('#', '') : "000000", // remove '#'
+          frame: frame ? "true" : "false",
+          frame_type: frame ? frameType : "none",
+        };
+
+        const effectsQuery = new URLSearchParams(effectsParams).toString();
+        const formData = new FormData();
+        formData.append('file', new Blob([byteObject], { type: "image/jpeg" }), "image.jpg");
+
+        const effectsResponse = await axios.post(
+          `${effectsSketchUrl}?${effectsQuery}`,
+          formData,
+          {
+            headers: { 'Content-Type': 'multipart/form-data' },
+            responseType: 'blob'  // Ensures that the response is handled as a Blob
+          }
+        );
+
+        if (effectsResponse.status === 200) {
+          const effectsSketchBlob = effectsResponse.data;
+          const effectsSketchUrl = window.URL.createObjectURL(new Blob([effectsSketchBlob]));
+          setEffectsSketchUrl(effectsSketchUrl);  // Save the sketch URL in state
+        } else {
+          console.error("Failed to create sketch with effects:", effectsResponse.status, effectsResponse.statusText);
+        }
+      };
+      setEffectLoading(false)
+    } catch (error) {
+      setEffectLoading(false)
+      console.error("An error occurred:", error);
+    }
+  };
+
+  // Handle download of the basic sketch
+  const handleDownload = () => {
+    if (basicSketchUrl) {
+      const link = document.createElement("a");
+      link.href = basicSketchUrl;
+      link.download = "basic_sketch.jpg";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      alert("First get a sketch");
+    }
+  };
+
+  const handleEffectDownload = () => {
+    if (effectsSketchUrl) {
+      const link = document.createElement("a");
+      link.href = effectsSketchUrl;
+      link.download = "effect_sketch.jpg";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      alert("First get a effect sketch");
+    }
+  };
+
+  // Update state when input range changes
+  const handleRangeChange = (setter) => (event) => {
+    setter(parseFloat(event.target.value).toFixed(1)); // Ensure that the value is up to 1 decimal place
+  };
+
+  // Log all range values to the console
+  const logRangeValues = () => {
+    console.log("Brightness:", brightness);
+    console.log("Sepia:", sepia);
+    console.log("Sharpen:", sharpen);
+    console.log("Vignette:", vignette);
+    console.log("Sepia Intensity:", sepiaIntensity);
+    console.log("Vignette Intensity:", vignetteIntensity);
+    console.log("Sharpen Intensity:", sharpenIntensity);
+    console.log("Sketch Style:", "abstract");
+    console.log("Border:", 'true');
+    console.log("Border Size:", '20');
+    console.log("Border Color:", '0,0,0');
+    console.log("Frame:", 'true');
+    console.log("Frame Type:", 'vintage');
+  };
+
+  // Handle voice commands
   useEffect(() => {
-    return () => {
-      if (sketchUrl) {
-        URL.revokeObjectURL(sketchUrl);
-      }
-    };
-  }, [sketchUrl]);
+    if (transcript.toLowerCase().includes("start sketching")) {
+      createBasicSketch();
+      resetTranscript();
+    } else if (transcript.toLowerCase().includes("start sketch with effects")) {
+      convertImageToSketchWithEffects();
+      resetTranscript();
+    } else if (transcript.toLowerCase().includes("save sketch")) {
+      handleDownload();
+      resetTranscript();
+    } else if (transcript.toLocaleLowerCase().includes("save effect sketch")) {
+      handleEffectDownload();
+      resetTranscript();
+    }
+    // eslint-disable-next-line
+  }, [transcript]);
+
+  if (!isSpeechRecognitionSupported) {
+    return <div>Speech Recognition is not supported in this browser.</div>;
+  }
 
   return (
-    <div className="flex flex-col h-screen py-14 laptop:px-20">
+    <div className="flex flex-col h-fit py-14 laptop:px-20">
       <div className="pt-5 text-4xl font-bold tracking-wide">
         Convert your Image to a Sketch
       </div>
-      <div className="flex items-center justify-center bg-green-400">
+      <div className="flex items-center justify-center">
         <div className="pt-2">
-          <input type="file" accept="image/*" onChange={handleImageUpload} className="mb-4" />
+          <input type="file" onChange={handleFileChange} />
         </div>
+        <p>{transcript}</p>
       </div>
-      <div className="flex flex-col laptop:flex-row bg-red-500 py-5">
-        <div className="flex flex-col bg-blue-100 laptop:w-1/2 justify-center items-center">
+      <div className="space-x-10 flex justify-center mt-10">
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={SpeechRecognition.startListening}>Use Voice Command</button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={SpeechRecognition.stopListening}>Stop Voice Command</button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={resetTranscript}>Reset Command</button>
+      </div>
+      <div className="flex flex-col laptop:flex-row py-5">
+        <div className="bg-green-200 w-fit"></div>
+        <div className="flex space-x-10 laptop:w-full justify-center items-center">
           <div className="border border-black my-2">
             <img
-              src={imageUrl || "https://via.placeholder.com/400x600"}
-              alt="Uploaded Preview"
+              src={selectedImageUrl || "https://placehold.co/960x540/000000/FFFFFF/png"}
+              alt="Selected Preview"
               className="object-cover"
-              style={{ width: '400px', height: '600px' }}
+              style={{ width: "960px", height: "540px" }}
             />
           </div>
-        </div>
-        <div className="flex flex-col bg-blue-100 laptop:w-1/2 justify-center items-center">
+
           <div className="border border-black my-2">
             <img
-              src={sketchUrl || "https://via.placeholder.com/400x600"}
+              src={basicSketchUrl || "https://placehold.co/960x540/000000/FFFFFF/png"}
               alt="Sketch Preview"
               className="object-cover"
-              style={{ width: '400px', height: '600px' }}
+              style={{ width: "960px", height: "540px" }}
+            />
+          </div>
+          <div className="border border-black my-2">
+            <img
+              src={effectsSketchUrl || "https://placehold.co/960x540/000000/FFFFFF/png"}
+              alt="Sketch Preview with Effects"
+              className="object-cover"
+              style={{ width: "960px", height: "540px" }}
             />
           </div>
         </div>
       </div>
-      <button
-        onClick={handleSketchConversion}
-        className="bg-gradient-to-r from-logoColor to-headingColor text-xl text-white rounded-lg px-6 py-4 mb-4"
-        disabled={loading}
-      >
-        {loading ? "Creating Sketch..." : "Create Sketch"}
-      </button>
+      <div className="space-x-10 flex justify-center mt-10">
+        <button
+          className="px-4 py-2 rounded-xl border-2 border-logoColor"
+          onClick={createBasicSketch}
+          disabled={loading}
+        >
+          {loading ? "Creating Sketch..." : "Create Sketch"}
+        </button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={convertImageToSketchWithEffects}>
+          {effectLoading ? "Creating Sketch with Effects..." : "Create Sketch with Effects"}
+        </button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={handleDownload}>Download Sketch</button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={handleEffectDownload}>Download Effect Sketch</button>
+        <button className="px-4 py-2 rounded-xl border-2 border-logoColor" onClick={logRangeValues}>Log Range Values</button>
+      </div>
+
+      {/* Add sliders for Brightness, Sepia, Sharpen, and Vignette */}
+      <div className="space-y-5 mt-10">
+      <div className="flex space-x-20">
+      <label className="w-36">Brightness</label>
+      <label>{brightness}</label>
+          <input
+            type="range"
+            min="0.1"
+            max="2.0"
+            step="0.1"
+            value={brightness}
+            onChange={handleRangeChange(setBrightness)}
+          />
+        </div>
+        <div className="flex space-x-20">
+          <label className="w-36">Sepia</label>
+          <label>{sepia}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={sepia}
+            onChange={handleRangeChange(setSepia)}
+          />
+        </div>
+        <div className="flex space-x-20">
+        <label className="w-36">Sepia Intensity</label>
+        <label>{sepiaIntensity}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={sepiaIntensity}
+            onChange={handleRangeChange(setSepiaIntensity)}
+          />
+        </div>
+        <div className="flex space-x-20">
+        <label className="w-36">Sharpen</label>
+        <label>{sharpen}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={sharpen}
+            onChange={handleRangeChange(setSharpen)}
+          />
+        </div>
+        <div className="flex space-x-20">
+        <label className="w-36">Sharpen Intensity</label>
+        <label>{sharpenIntensity}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={sharpenIntensity}
+            onChange={handleRangeChange(setSharpenIntensity)}
+          />
+        </div>
+        <div className="flex space-x-20">
+        <label className="w-36">Vignette</label>
+        <label>{vignette}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={vignette}
+            onChange={handleRangeChange(setVignette)}
+          />
+        </div>
+        <div className="flex space-x-20">
+        <label className="w-36">Vignette Intensity</label>
+          <label>{vignetteIntensity}</label>
+          <input
+            type="range"
+            min="0.0"
+            max="1.0"
+            step="0.1"
+            value={vignetteIntensity}
+            onChange={handleRangeChange(setVignetteIntensity)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
